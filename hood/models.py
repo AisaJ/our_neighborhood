8c6d21e4c55327b9e8f8c577d3a40f28e2cf531e
Aisa <aisajemila@gmail.com>
2019-05-31 17:50:49 +0300

    Created neighborhood model class.

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 # Create your models here.
+
+class Neighborhood(models.Model):
+  name=models.CharField(max_length=50)
+  hood_image=models.ImageField(upload_to='hood/',default='city.jpeg')
+  members=models.IntegerField()
+
+  def __str__(self):
+    self.name
